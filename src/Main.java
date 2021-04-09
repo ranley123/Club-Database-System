cb8dd95cbe842c5d6a5efba6f87c46194fd6c52d
Ranley123 <hy30@st-andrews.ac.uk>
2021-04-09 18:11:12 +0800

    2021-4-6

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,28 @@ import java.sql.*;
 public class Main {
     Connection dbConnection = null;
     public static void main(String[] args){
-
         System.out.println("Database Loading...");
+        Main main = new Main();
+        main.initDatabase();
     }
 
-    private void initDatabase() throws SQLException{
+    private void initDatabase(){
         dbConnection = null;
 
         try{
-            String dbUrl = "jdbc:mariadb:club";
+            String dbUrl = "jdbc:mariadb://localhost/club";
+//            String uname = "hy30";
+//            String passwd = "Yh20000109000537";
             dbConnection = DriverManager.getConnection(dbUrl);
-        }
-        catch (SQLException e){
-            System.out.println(e.getMessage());
-        }
-
-        Statement statement  = null;
-
-        try{
+            Statement statement  = null;
             statement = dbConnection.createStatement();
             statement.executeUpdate("DROP TABLE IF EXISTS club");
             statement.executeUpdate("CREATE TABLE club (id int, question VARCHAR(100))");
             statement.close();
+            System.out.println("hi");
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
         }
     }
 }
